b7adccc8941591340dc0d83ff042f3424b4b509e
Manish D'Silva <45946138+manishdsilva@users.noreply.github.com>
2019-06-24 19:19:23 +0530

    Create Translation_Z.lua
    This is a lua script which will depict translation along Z-Axis on V-Rep .Can be noticed that there are just a few sign changes as compared to Rotation about Z-Axis

---
+++ Translation_Z.lua
@@ -0,0 +1,35 @@
+function sysCall_init()
+
+    Servo1=sim.getObjectHandle('Servo1_rotor') 
+    Servo2=sim.getObjectHandle('Servo2_rotor') 
+ 
+    Servo3=sim.getObjectHandle('Servo3_rotor')
+    Servo4=sim.getObjectHandle('Servo4_rotor') 
+
+    Servo5=sim.getObjectHandle('Servo5_rotor')  
+    Servo6=sim.getObjectHandle('Servo6_rotor')
+    sphere  =sim.getObjectHandle('Sphere')
+
+    pf    =sim.getObjectHandle('Platform')
+    Dirn =1 
+    i=1
+    
+end
+
+function sysCall_actuation()
+    
+    if((math.floor(i/100)%2)==1) then 
+    Dirn = -1
+    else
+    Dirn = 1
+    end
+    Angle= 20
+    sim.setJointTargetPosition(Servo5,-Dirn*Angle*math.pi/180)
+    sim.setJointTargetPosition(Servo6,Dirn*Angle*math.pi/180)
+    sim.setJointTargetPosition(Servo1,Dirn*Angle*math.pi/180)
+    sim.setJointTargetPosition(Servo2,-Dirn*Angle*math.pi/180)
+    sim.setJointTargetPosition(Servo3,-Dirn*Angle*math.pi/180)
+    sim.setJointTargetPosition(Servo4,Dirn*Angle*math.pi/180)
+    i=i+2
+end
+
